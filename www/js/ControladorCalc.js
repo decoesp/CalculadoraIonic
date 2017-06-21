28c639e64ec9084b71449fcac548e34ca6994181
Italo Gregorio <italo-07@hotmail.com>
2017-06-21 18:56:07 -0300

    Commit inicial

---
+++ www/js/ControladorCalc.js
@@ -0,0 +1,60 @@
+angular.module('starter')
+.controller('ControladorCalc', function($scope){
+
+  $scope.visor = 0;
+  var arrayNum = [];
+  var num = 0;
+  var oper = '';
+
+  $scope.apagar = function(){
+    $scope.visor = 0;
+    oper = '';
+    arrayNum = [];
+  }
+
+  $scope.numClicado = function(n){
+    arrayNum.push(n);
+    $scope.visor = arrayNum.join('')*1;
+  }
+
+  $scope.operacao = function(op){
+    if(oper !== ''){
+      $scope.resultado();
+    }
+    num = $scope.visor;
+    arrayNum = [];
+    oper = op;
+  }
+
+  $scope.resultado = function(){
+    if(oper === '+'){
+      $scope.visor += num;
+    }
+    else if(oper === '-'){
+      $scope.visor = num - $scope.visor; // não continua a subtração ao apertar o igual duas vezes
+    }
+    else if(oper === '*'){
+      $scope.visor *= num;
+    }
+    else if(oper === '/'){
+      $scope.visor = num / $scope.visor; // mesmo bug da subtração
+    }
+  }
+
+  $scope.raizQuadrada = function(){
+    $scope.visor = Math.sqrt($scope.visor);
+  }
+
+  $scope.calcularSeno = function(){
+    $scope.visor = Math.sin($scope.visor);
+  }
+
+  $scope.calcularCosseno = function(){
+    $scope.visor = Math.cos($scope.visor);
+  }
+
+  $scope.pi = function(){
+    $scope.visor *= Math.PI;
+  }
+
+})
